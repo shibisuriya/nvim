63d0e78521f4e910ca70bbf6cdddb1e2b190655d
Shibi <shibisuriya.work@gmail.com>
2024-04-09 20:13:44 +0530

    fix: update the theme

--- init.lua
+++ init.lua
@@ -175,7 +175,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -250,31 +250,7 @@ require('lazy').setup({
       end,
     },
   },
-
-  {
-    -- Theme inspired by Atom
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function(opts)
-      vim.cmd.colorscheme("tokyonight")
-
-      -- To make the color of the terminal transparent.
-      vim.api.nvim_set_hl(0, "NormalNc", {
-        bg = "none"
-      })
-
-      vim.api.nvim_set_hl(0, "Normal", {
-        bg = "none"
-      })
-
-      vim.api.nvim_set_hl(0, "NormalFloat", {
-        bg = "none"
-      })
-
-      vim.api.nvim_set_hl(0, 'LineNr', { fg = "darkgray" })
-    end,
-  },
-
+  { "rose-pine/neovim",     name = "rose-pine" },
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -702,14 +678,12 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  prettierd = {},
-  prettier = {},
+  pylsp = {},
   cssls = {},
   cssmodules_ls = {},
   tsserver = {},
-  ruff = {},
-  isort = {},
-  black = {},
+  eslint = {},
+  stylelint_lsp = {},
 
   -- clangd = {},
   -- gopls = {},
@@ -959,3 +933,72 @@ vim.api.nvim_set_keymap('n', '<leader>gg', ':tabnew | G<CR>', { noremap = true, 
 
 vim.api.nvim_set_keymap('n', '<leader>dv', ':tab  Gvdiffsplit<CR>', { noremap = true, silent = true })
 require('leap').create_default_mappings()
+
+require("rose-pine").setup({
+  variant = "auto",      -- auto, main, moon, or dawn
+  dark_variant = "moon", -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
+  enable = {
+    terminal = true,
+    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    migrations = true,        -- Handle deprecated options automatically
+  },
+
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = false,
+  },
+
+  groups = {
+    border = "muted",
+    link = "iris",
+    panel = "surface",
+
+    error = "love",
+    hint = "iris",
+    info = "foam",
+    note = "pine",
+    todo = "rose",
+    warn = "gold",
+
+    git_add = "foam",
+    git_change = "rose",
+    git_delete = "love",
+    git_dirty = "rose",
+    git_ignore = "muted",
+    git_merge = "iris",
+    git_rename = "pine",
+    git_stage = "iris",
+    git_text = "rose",
+    git_untracked = "subtle",
+
+    h1 = "iris",
+    h2 = "foam",
+    h3 = "rose",
+    h4 = "gold",
+    h5 = "pine",
+    h6 = "foam",
+  },
+
+  highlight_groups = {
+    -- Comment = { fg = "foam" },
+    -- VertSplit = { fg = "muted", bg = "muted" },
+  },
+
+  before_highlight = function(group, highlight, palette)
+    -- Disable all undercurls
+    -- if highlight.undercurl then
+    --     highlight.undercurl = false
+    -- end
+    --
+    -- Change palette colour
+    -- if highlight.fg == palette.pine then
+    --     highlight.fg = palette.foam
+    -- end
+  end,
+})
+
+vim.cmd("colorscheme rose-pine")
