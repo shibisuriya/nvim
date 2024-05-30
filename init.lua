4878420635cc48a13ad2ab69e4887274802c57ae
Shibi <shibisuriya.work@gmail.com>
2024-05-30 20:50:07 +0530

    added obsidian to nvim

--- init.lua
+++ init.lua
@@ -105,6 +105,39 @@ require('lazy').setup({
     },
   },
   {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Desktop/exit-pod/personal-notes",
+        },
+        {
+          name = "work",
+          path = "~/Desktop/exit-pod/office-notes",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     opts = {
       on_attach = function(bufnr)
@@ -971,7 +1004,7 @@ require("rose-pine").setup({
   styles = {
     bold = true,
     italic = true,
-    transparency = false,
+    transparency = true,
   },
 
   groups = {
@@ -1024,3 +1057,4 @@ require("rose-pine").setup({
 })
 
 vim.cmd("colorscheme rose-pine")
+vim.opt.conceallevel = 1
