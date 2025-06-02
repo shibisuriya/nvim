b28217cb95972b2ba61a69730584c8667f0eae7f
Shibi Suriya P <shibisuriya.work@gmail.com>
2025-06-02 23:45:27 +0530

    fix: added mason-tool-installer

--- init.lua
+++ init.lua
@@ -110,6 +110,80 @@ require('lazy').setup({
     },
   },
   {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      -- a list of all tools you want to ensure are installed upon
+      -- start
+      ensure_installed = {
+        'tailwindcss',
+        'bashls',
+        'stylua',
+        'prettier',
+        'prettierd',
+        'clangd',
+        'cssls',
+        'cssmodules_ls',
+        'dockerls',
+        'eslint',
+        'html',
+        'jdtls',
+        'lua_ls',
+        'mdx_analyzer',
+        'pylsp',
+        'nil_ls',
+        'ruff',
+        'stylelint_lsp',
+        'ts_ls',
+        'yamlls',
+        'google-java-format',
+        'clang-format',
+        'beautysh',
+        'xmlformatter',
+        -- 'goimports',
+        -- 'gofmt'
+      },
+
+      -- if set to true this will check each tool for updates. If updates
+      -- are available the tool will be updated. This setting does not
+      -- affect :MasonToolsUpdate or :MasonToolsInstall.
+      -- Default: false
+      auto_update = false,
+
+      -- automatically install / update on startup. If set to false nothing
+      -- will happen on startup. You can use :MasonToolsInstall or
+      -- :MasonToolsUpdate to install tools and check for updates.
+      -- Default: true
+      run_on_start = true,
+
+      -- set a delay (in ms) before the installation starts. This is only
+      -- effective if run_on_start is set to true.
+      -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+      -- Default: 0
+      start_delay = 3000, -- 3 second delay
+
+      -- Only attempt to install if 'debounce_hours' number of hours has
+      -- elapsed since the last time Neovim was started. This stores a
+      -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+      -- This is only relevant when you are using 'run_on_start'. It has no
+      -- effect when running manually via ':MasonToolsInstall' etc....
+      -- Default: nil
+      debounce_hours = 5, -- at least 5 hours between attempts to install/update
+
+      -- By default all integrations are enabled. If you turn on an integration
+      -- and you have the required module(s) installed this means you can use
+      -- alternative names, supplied by the modules, for the thing that you want
+      -- to install. If you turn off the integration (by setting it to false) you
+      -- cannot use these alternative names. It also suppresses loading of those
+      -- module(s) (assuming any are installed) which is sometimes wanted when
+      -- doing lazy loading.
+      integrations = {
+        ['mason-lspconfig'] = true,
+        -- ['mason-null-ls'] = true,
+        -- ['mason-nvim-dap'] = true,
+      },
+    },
+  },
+  {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
@@ -651,7 +725,7 @@ vim.defer_fn(function()
       'markdown',
       'markdown_inline',
       'json',
-      'nix'
+      'nix',
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
@@ -797,26 +871,7 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  tailwindcss = {},
-  bashls = {},
-  clangd = {},
-  cssls = {},
-  cssmodules_ls = {},
-  dockerls = {},
-  eslint = {},
-  html = {},
-  jdtls = {},
-  lua_ls = {},
-  mdx_analyzer = {},
-  pylsp = {},
-  nil_ls = {},
-  ruff = {},
-  stylelint_lsp = {},
-  ts_ls = {},
-  yamlls = {},
-
   html = { filetypes = { 'html', 'twig', 'hbs' } },
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -838,7 +893,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+  -- ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
